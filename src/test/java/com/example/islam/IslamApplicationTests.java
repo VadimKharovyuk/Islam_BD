f45dacf1fe8709de5d8dfc89f0d741e6c02c999d
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2025-09-20 23:32:32 +0300

    Initial commit

---
+++ src/test/java/com/example/islam/IslamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.islam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IslamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
